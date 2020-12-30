6ade78d2d09dabe936981e7e07291a235ff7c57c
Amit Garu <amitgaru2@gmail.com>
2020-12-30 13:53:17 +0545

    version config update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as readme_file:
 
 setup(
     name="nepali-datetime",
-    version="1.0.5",
+    version="1.0.5.1",
     description="Datetime module that operates on Bikram Sambat & Nepal time.",
     long_description=README,
     long_description_content_type="text/markdown",
